cf332e31f39fcaf36f3b33347c0a6886b17ce3ca
Leandra Tejedor <lmtejedor@gmail.com>
2014-01-16 19:38:34 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Winterest::Application.config.secret_key_base = '3c99927de959f34ed2c28016725b38710d2d21ac6d2a266c31e11ac5cfe47655a6d77f838c4e495ffdd06239aaadc59c4a5c254ccf501a42d35d8e47485d576a'
