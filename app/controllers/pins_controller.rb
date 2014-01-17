e0d61d57ed440b142724cddd048e6c015d0b391f
Leandra Tejedor <lmtejedor@gmail.com>
2014-01-16 20:53:09 -0500

    Add the pins view

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,8 +1,27 @@
 class PinsController < ApplicationController
 	def index
+		@pins = Pin.all
 	end
 
 	def new
 		@pin = Pin.new
 	end
+
+	def create
+		@pin = Pin.new(pin_params)
+
+		if @pin.save
+			redirect_to root_path
+		else
+			render "this was an error!"
+			redirect_to new_pin_path
+		end
+	end
+
+	private
+
+	def pin_params
+		params.require(:pin).permit(:title, :description, :source)
+	end
+
 end
